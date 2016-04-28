6bc96a76582d704419d36da0f69441e48f989835
Phuttipong Aims <phuttipong@tst.co.th>
2016-04-28 17:32:02 +0700

    feat: add todo list web ui with React.

---
+++ ReactUi/app/CheckList.js
@@ -0,0 +1,29 @@
+/*
+ * Created by phuttipong on 28/4/2559.
+ */
+import React, {Component} from 'react';
+
+class CheckList extends Component {
+    render() {
+        let tasks = this.props.tasks.map((task) => (
+            <li key={task.id} className="checklist__task">
+                <input type="checkbox" checked={task.done}
+                       readOnly="true"/> {/*read-only controlled input UI won't changed by user*/}
+                {" "}
+                {task.name}
+                {" "}
+                <a href="#" className="checklist__task--remove"/>
+            </li>
+        ));
+
+        return (
+            <div className="checklist">
+                <ul>{tasks}</ul>
+                <input type="text"
+                       className="checklist--add-task"
+                       placeholder="Type then hit Enter to add a task"/>
+            </div>
+        );
+    }
+}
+export default CheckList;
---
+++ ReactUi/app/List.js
@@ -0,0 +1,26 @@
+/*
+ * Created by phuttipong on 28/4/2559.
+ */
+import React, {Component} from 'react';
+import Card from './Card';
+
+class List extends Component {
+    render() {
+        var cards = this.props.cards.map((card) => {
+            return <Card key={card.id}
+                         id={card.id}
+                         title={card.title}
+                         description={card.description}
+                         color={card.color}
+                         tasks={card.tasks}/>
+        });
+        return (
+            <div className="list">
+                <h1>{this.props.title}</h1>
+                {cards}
+            </div>
+        );
+    }
+}
+
+export default List;
