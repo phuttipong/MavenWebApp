b7966a4749ef9739403796811f335e4453855824
Phuttipong Aims <phuttipong@tst.co.th>
2016-05-11 15:27:41 +0700

    docs: add document to gulpfile.js

--- WebixUi/gulpfile.js
+++ WebixUi/gulpfile.js
@@ -27,23 +27,38 @@ gulp.task('js', function () {
 });
 
 function build_js() {
+    //create list of module-name in views folder
     var views = glob.sync("views/**/*.js").map(function (value) {
-        return value.replace(".js", "");
+        return value.replace(".js", ""); //Relative module names are relative to other names, not paths
     });
-
+    //create list of module-name in views folder
     var locales = glob.sync("locales/**/*.js").map(function (value) {
-        return value.replace(".js", "");
+        return value.replace(".js", "");//Relative module names are relative to other names, not paths
     });
 
     return rjs({
+        //the root path to use for all module lookups.
+        // So in the above example, "my/module"'s script tag will have a src="/another/path/my/module.js".
+        // baseUrl is not used when loading plain .js files (indicated by a dependency string starting with a slash, has a protocol, or ends in .js)
         baseUrl: './',
         out: 'app.js',
+        //insertRequire build setting to insert a require call for the main module --
+        // that serves the same purpose of the initial require() call that data-main does.
         insertRequire: ["app"],
+        //path mappings for module names not found directly under baseUrl.
+        // The path settings are assumed to be relative to baseUrl,
+        // unless the paths setting starts with a "/" or has a URL protocol in it ("like http:").
+        // Using the above sample config, "some/module"'s script tag will be src="/another/path/some/v1.0/module.js"
         paths: {
             "locale": "empty:",
             "text": 'libs/text'
         },
+        //An array of dependencies to load. Useful when require is defined as a config object before require.js is loaded,
+        // and you want to specify dependencies to load as soon as require() is defined. Using deps is just like doing a require([]) call.
         deps: ["app"],
+        //A replacement [AMD] loader for[RequireJS].
+        // It provides a minimal AMD API footprint that includes [loader plugin] support.
+        // Only useful for built/bundled AMD modules, does not do dynamic loading.
         include: ["libs/almond/almond.js"].concat(views).concat(locales)
     })
         .pipe(_if(debug_export, sourcemaps.init()))
@@ -59,6 +74,7 @@ gulp.task("clean", function () {
 gulp.task('build', ["clean"], function () {
     var build = (new Date()) * 1;
 
+    //event-stream's merge function merge streams from different methods.
     return require('event-stream').merge(
         build_js(),
         build_css(),
@@ -68,13 +84,12 @@ gulp.task('build', ["clean"], function () {
         //index
         gulp.src("./index.html")
             .pipe(replace('data-main="app" src="libs/requirejs/require.js"', 'src="app.js"'))
-            .pipe(replace('<script type="text/javascript" src="libs/less.min.js"></script>', ''))
+            .pipe(replace('<script type="text/javascript" src="libs/less/dist/less.min.js"></script>', ''))
             .pipe(replace(/rel\=\"stylesheet\/less\" href=\"(.*?)\.less\"/g, 'rel="stylesheet" href="$1.css"'))
             .pipe(replace(/\.css\"/g, '.css?' + build + '"'))
             .pipe(replace(/\.js\"/g, '.js?' + build + '"'))
             .pipe(replace("require.config", "webix.production = true; require.config"))
             .pipe(replace(/libs\/webix\/codebase\//g, '//cdn.webix.com/edge/'))
-
             .pipe(gulp.dest("./deploy/")),
         //server
         gulp.src(["./server/**/*.*",
@@ -84,7 +99,6 @@ gulp.task('build', ["clean"], function () {
     );
 
 });
-
 
 gulp.task('lint', function () {
     return gulp.src(['./views/**/*.js', './helpers/**/*.js', './models/**/*.js', './*.js', "!./\.jshintrc"])
